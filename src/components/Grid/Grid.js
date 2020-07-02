f3f9e96870cab2e53d09eeb79c14082fbbceeec3
Advitya sharma <54709416+Advitya-sharma@users.noreply.github.com>
2020-07-02 09:05:22 +0000

    fixing button style

--- src/components/Grid/Grid.js
+++ src/components/Grid/Grid.js
@@ -1,52 +1,87 @@
 import React, { Component } from "react";
+import "./buttons.css";
+
+const button = [
+  {
+    id: 1,
+    title: "1"
+  },
+  {
+    id: 2,
+    title: "2"
+  },
+  {
+    id: 3,
+    title: "3"
+  },
+  {
+    id: 4,
+    title: "4"
+  },
+  {
+    id: 5,
+    title: "5"
+  },
+  {
+    id: 6,
+    title: "6"
+  },
+  {
+    id: 7,
+    title: "7"
+  },
+  {
+    id: 8,
+    title: "8"
+  },
+  {
+    id: 9,
+    title: "9"
+  },
+  {
+    id: "zero",
+    title: "0"
+  },
+  {
+    id: "clear",
+    title: "AC"
+  },
+  {
+    id: "equal",
+    title: "="
+  },
+  {
+    id: 1,
+    title: "1"
+  },
+  {
+    id: "plus",
+    title: "+"
+  },
+  {
+    id: "minus",
+    title: "-"
+  },
+  {
+    id: "multiply",
+    title: "*"
+  },
+  {
+    id: "divide",
+    title: "/"
+  }
+];
 
 export class Grid extends Component {
   render() {
     return (
-      <div id="grid" style={gridStyle}>
-        <button id="AC" style={acStyle} />
-        <button />
-        <button />
-        <button />
-        <button />
-        <button />
-        <button />
-        <button />
-        <button />
-        <button />
-        <button />
-        <button />
-        <button />
-        <button />
-        <button />
-        <button id="equal" style={equalStyle} />
-        <button id="zero" value="33" style={zeroStyle} />
+      <div id="grid">
+        {button.map(button => {
+          return <button id={button.id}>{button.title}</button>;
+        })}
       </div>
     );
   }
 }
-
-const gridStyle = {
-  display: "grid",
-  gridTemplateColumns: "75px 75px 75px 75px",
-  gridTemplateRows: "84px 84px 84px 84px 84px"
-};
-
-const acStyle = {
-  backgroundColor: "pink",
-  gridColumn: "1/3"
-};
-
-const zeroStyle = {
-  backgroundColor: "pink",
-  gridColumn: "1/3",
-  gridRow: "5/6"
-};
-
-const equalStyle = {
-  backgroundColor: "pink",
-  gridColumn: "4/5",
-  gridRow: "4/6"
-};
 
 export default Grid;
