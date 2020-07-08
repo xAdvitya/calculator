63633cedf39a698c9c465c6b706210098b70070d
Advitya sharma <54709416+Advitya-sharma@users.noreply.github.com>
2020-07-08 12:28:27 +0000

    💅 color modification

--- src/App.js
+++ src/App.js
@@ -1,12 +1,18 @@
 import React, { Component } from "react";
 import Board from "./components/Board";
 import "./styles.css";
+import Header from "./components/header/Header";
 
 export class App extends Component {
   render() {
     return (
-      <div className="App">
-        <Board />
+      <div>
+        <Header />
+        <div id="flex">
+          <div className="App">
+            <Board />
+          </div>
+        </div>
       </div>
     );
   }
---
+++ src/components/header/Header.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./header.css";
+
+function Header() {
+  return (
+    <div id="header">
+      <i className="fa fa-github" /> Calculator
+    </div>
+  );
+}
+
+export default Header;
